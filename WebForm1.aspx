80fed1fe5633638efae5c17efe89f65fc64ad089
Davide <DavideBuglione@hotmail.com>
2024-02-14 17:09:41 +0100

    Aggiungere i file di progetto.

---
+++ WebForm1.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EsercizioW3D3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       <div>
+           <asp:TextBox ID="TextNome" runat="server"></asp:TextBox>
+           <asp:TextBox ID="TextCognome" runat="server"></asp:TextBox>
+          <asp:RadioButtonList ID="RadioButtonList1" runat="server">
+    <asp:ListItem Text="SalaA" Value="SalaA"></asp:ListItem>
+    <asp:ListItem Text="SalaB" Value="SalaB"></asp:ListItem>
+    <asp:ListItem Text="SalaC" Value="SalaC"></asp:ListItem>
+</asp:RadioButtonList>
+           <asp:CheckBox ID="Sconto" runat="server" />
+           <asp:Label ID="Label1" runat="server" Text="Sconto"></asp:Label>
+
+       </div>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Compra Biglietto" />
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EsercizioW3D3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["Tickets"].ConnectionString; 
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            try
+            {
+                string valoreSala = RadioButtonList1.SelectedValue;
+                bool isChecked = Sconto.Checked;
+
+                conn.Open();
+                Response.Write("La connessione è stata correttamente aperta");
+
+                SqlCommand cmdInser = new SqlCommand("INSERT INTO Ticket (Nome, Cognome, Sala, Tipo) VALUES (@Nome, @Cognome, @Sala, @Tipo)", conn);
+                cmdInser.Parameters.AddWithValue("@Nome", TextNome.Text);
+                cmdInser.Parameters.AddWithValue("@Cognome", TextCognome.Text);
+                cmdInser.Parameters.AddWithValue("@Sala", valoreSala);
+                cmdInser.Parameters.AddWithValue("@Tipo", isChecked);
+                int affectedRows = cmdInser.ExecuteNonQuery();
+
+                if (affectedRows > 0)
+                {
+                    Response.Write("Inserimento Riuscito");
+                }
+                else
+                {
+                    Response.Write("Inserimento non riuscito");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("La connessione non può essere aperta: " + ex.Message); 
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
